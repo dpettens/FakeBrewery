04e6542873ff5d69572f42969455caced23b400d
Denis Pettens <denis.pettens@gmail.com>
2020-07-19 23:03:06 +0200

    feat: initialize db with ef core

---
+++ src/FakeBrewery.Infra.Data/BreweryContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using FakeBrewery.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FakeBrewery.Infra.Data
+{
+    public class BreweryContext : DbContext
+    {
+        public DbSet<Beer> Beers { get; set; }
+        public DbSet<Brewery> Breweries { get; set; }
+
+        public BreweryContext(DbContextOptions<BreweryContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Data configuring
+
+            modelBuilder.Entity<Brewery>()
+                .HasMany(br => br.Beers)
+                .WithOne(b => b.Brewery);
+
+            // Data seeding
+
+            modelBuilder.Entity<Brewery>().HasData(
+                new Brewery() { Id = new Guid("cd876cae-ff5b-429d-970b-11af42900f1b"), Name = "Abbaye de Leffe" },
+                new Brewery() { Id = new Guid("91bff65f-96f2-4bd1-8b2e-eeaef2b46555"), Name = "Abbaye d'Orval" },
+                new Brewery() { Id = new Guid("a5a1d759-7471-431e-92c0-0f40c35bc855"), Name = "Abbaye de Westmalle" },
+                new Brewery() { Id = new Guid("d661055d-5c38-4201-b937-79b1b5d77f8f"), Name = "Brasserie Bosteels" }
+            );
+        }
+    }
+}
--- src/FakeBrewery.WebApi/Startup.cs
+++ src/FakeBrewery.WebApi/Startup.cs
@@ -1,5 +1,7 @@
+using FakeBrewery.Infra.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -18,6 +20,9 @@ namespace FakeBrewery.WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<BreweryContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("BreweryContext")));
+
             services.AddControllers();
         }
 
