1a037c9f57387e5c654a04aa320955ca711ceee6
Denis Pettens <denis.pettens@gmail.com>
2020-07-21 04:08:28 +0200

    feat: add Estimate and Order dtos

---
+++ src/FakeBrewery.Application/Dtos/Order.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FakeBrewery.Application.Dtos
+{
+    public class Order
+    {
+        public Guid WholesalerId { get; set; }
+        public IEnumerable<OrderItem> Items { get; set; } = new List<OrderItem>();
+    }
+}
---
+++ src/FakeBrewery.Application/Dtos/OrderItem.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace FakeBrewery.Application.Dtos
+{
+    public class OrderItem
+    {
+        public Guid BeerId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
